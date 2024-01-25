d25cee793559edfbeeebbde71e55f940abd95b83
Murat Seçgin <muratsecgin@Murat-MacBook-Air.local>
2024-01-25 15:58:56 +0300

    fetching data with fetch api

--- main.js
+++ main.js
@@ -5,24 +5,51 @@ console.log("window.location.hostname", window.location.hostname)
 
 const hashLinks = document.querySelectorAll(".hash-route-link")
 
+const app = document.querySelector("#app")
+
 const routes = {
   "": {
     title: "Ana Sayfa",
+    data: "./home.html",
+    isApi: false,
   },
   "#todos": {
-    title: "Todo List"
+    title: "Todo List",
+    data: "https://jsonplaceholder.typicode.com/todos/",
+    isApi: true,
   },
   "#about": {
-    title: "About Us"
+    title: "About Us",
+    data: "./about-us.html",
+    isApi: false,
   },
   "#contact": {
-    title: "Contact"
+    title: "Contact",
+    data: "./contact.html",
+    isApi: false,
   },
 }
 
 function checkRoute(hash = window.location.hash) {
   // console.log(routes[hash])
   document.title = routes[hash].title
+  const dataUrl = routes[hash].data
+  console.log(dataUrl)
+  isApi = routes[hash].isApi
+  if (isApi) {
+    app.innerHTML = ""
+    const createUl = document.createElement("ul")
+    app.append(createUl)
+    fetch(dataUrl).then(res => res.json()).then(data => {
+      data.forEach(item => {
+        const createLi = document.createElement("li")
+        createLi.innerHTML = item.title
+        createUl.append(createLi)
+      })
+    })
+  } else if (!isApi) {
+    fetch(dataUrl).then(res => res.text()).then(data => app.innerHTML = data)
+  }
 }
 
 hashLinks.forEach(item => item.addEventListener("click", (event) => {
